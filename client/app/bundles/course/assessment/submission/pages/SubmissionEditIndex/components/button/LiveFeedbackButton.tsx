5160f29eb36405fe87d7c15680f25ff89607b3be
Cuong Phung <cuongphungmanh@gmail.com>
2024-11-11 20:51:48 +0800

    refactor(live-feedback): remove unused dispatch logic from LiveFeedbackButton

--- client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/components/button/LiveFeedbackButton.tsx
+++ client/app/bundles/course/assessment/submission/pages/SubmissionEditIndex/components/button/LiveFeedbackButton.tsx
@@ -1,19 +1,13 @@
 import { FC } from 'react';
 import { Button } from '@mui/material';
 
-import {
-  generateLiveFeedback,
-  initializeLiveFeedback,
-} from 'course/assessment/submission/actions/answers';
-import { getAssessment } from 'course/assessment/submission/selectors/assessments';
 import { getLiveFeedbacks } from 'course/assessment/submission/selectors/liveFeedbacks';
 import { getQuestionFlags } from 'course/assessment/submission/selectors/questionFlags';
 import { getQuestions } from 'course/assessment/submission/selectors/questions';
 import { getSubmission } from 'course/assessment/submission/selectors/submissions';
 import translations from 'course/assessment/submission/translations';
 import LoadingIndicator from 'lib/components/core/LoadingIndicator';
-import { getSubmissionId } from 'lib/helpers/url-helpers';
-import { useAppDispatch, useAppSelector } from 'lib/hooks/store';
+import { useAppSelector } from 'lib/hooks/store';
 import useTranslation from 'lib/hooks/useTranslation';
 
 interface Props {
@@ -24,22 +18,15 @@ interface Props {
 const LiveFeedbackButton: FC<Props> = (props) => {
   const { t } = useTranslation();
   const { questionId, onClick } = props; // Destructure onClick prop
-
-  const dispatch = useAppDispatch();
-
-  const assessment = useAppSelector(getAssessment);
   const submission = useAppSelector(getSubmission);
   const questions = useAppSelector(getQuestions);
   const questionFlags = useAppSelector(getQuestionFlags);
   const liveFeedback = useAppSelector(getLiveFeedbacks);
 
-  const submissionId = getSubmissionId();
-
   const question = questions[questionId];
 
-  const { questionIds } = assessment;
   const { graderView } = submission;
-  const { answerId, attemptsLeft } = question;
+  const { attemptsLeft } = question;
   const { isResetting } = questionFlags[questionId] || {};
 
   const isRequestingLiveFeedback =
@@ -49,27 +36,6 @@ const LiveFeedbackButton: FC<Props> = (props) => {
   const isPollingLiveFeedback =
     (liveFeedback?.feedbackByQuestion?.[questionId]?.pendingFeedbackToken ??
       false) !== false;
-
-  const onGenerateLiveFeedback = (): void => {
-    const questionIndex = questionIds.findIndex((id) => id === questionId) + 1;
-    const successMessage = t(translations.liveFeedbackSuccess, {
-      questionIndex,
-    });
-    const noFeedbackMessage = t(translations.liveFeedbackNoneGenerated, {
-      questionIndex,
-    });
-
-    dispatch(initializeLiveFeedback(questionId));
-    dispatch(
-      generateLiveFeedback({
-        submissionId,
-        answerId,
-        questionId,
-        successMessage,
-        noFeedbackMessage,
-      }),
-    );
-  };
 
   // TODO: update logic pending #7418: allow [Live feedback] on all programming questions
 
